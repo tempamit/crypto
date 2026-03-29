c86e8cebfd29eac2a6ce354ae89c790ff6f703e9
tempamit <tempamit@gmail.com>
2026-03-30 02:07:29 +0530

    Fixed Dockerfile and ignored venv

--- auto_poster.py
+++ auto_poster.py
@@ -22,7 +22,7 @@ WP_TAGS_URL = "https://crypto.ipds.cloud/wp-json/wp/v2/tags"
 WP_USER = "adminipds"
 WP_APP_PASSWORD = "fwjS Kt1y aHsC iTx4 jxtA EIvl" 
 
-GEMINI_API_KEY = "AIzaSyCxVJ9JWH-AjLOqd4Fo_-NiWMiFyC_nWNU"
+GEMINI_API_KEY = "AIzaSyDs4cQA6ln7G7tOmaafKozc_Z9pUSxfji4"
 JSON_KEY_FILE = "service_account.json" 
 DB_FILE = "crypto_processed.db" # CRITICAL: Renamed so it doesn't conflict
 
